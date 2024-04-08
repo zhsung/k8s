571f670823fdd3443fe43030a64963d16a81425a
zhsung <zzin1016@naver.com>
2024-04-09 02:26:49 +0900

    containerd 설정

--- 4_install_containerd.sh
+++ 4_install_containerd.sh
@@ -37,3 +37,31 @@ sudo mkdir -p /etc/containerd
 
 sudo containerd config default|sudo tee /etc/containerd/config.toml
 
+echo '
+# /etc/containerd/config.toml에서 disabled_plugins 라인을 비활성화하여 CRI 인터페이스를 활성화합니다.
+sudo vim /etc/containerd/config.toml
+# /etc/containerd/config.toml
+...
+disabled_plugins = ["cri"]
+=> 
+# disabled_plugins = ["cri"]
+...
+
+# systemd를 cgroup driver로 사용하기
+> vim /etc/containerd/config.toml
+SystemdCgroup = true
+===
+[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
+  ...
+  [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+    SystemdCgroup = true
+
+
+수정 후 containerd 재시작
+
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+systemctl status  containerd
+
+
+'
